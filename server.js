64d7b46e2e9d0aff734e98126a0ec8e366d88be1
masudrana08 <rana028511@gmail.com>
2020-10-15 20:53:06 +0600

    aa

--- server.js
+++ server.js
@@ -14,10 +14,10 @@ const admin = require('firebase-admin');
 const serviceAccount={
     "type": "service_account",
     "project_id": "creative-agency-fullstack",
-    "private_key_id": process.env.PRIVATE_KEY_ID,
-    "private_key": process.env.PRIVATE_KEY,
-    "client_email": process.env.CLIENT_EMAIL,
-    "client_id": process.env.CLIENT_ID,
+    "private_key_id": `${process.env.PRIVATE_KEY_ID}`,
+    "private_key": `${process.env.PRIVATE_KEY}`,
+    "client_email": `${process.env.CLIENT_EMAIL}`,
+    "client_id": `${process.env.CLIENT_ID}`,
     "auth_uri": "https://accounts.google.com/o/oauth2/auth",
     "token_uri": "https://oauth2.googleapis.com/token",
     "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
@@ -26,7 +26,7 @@ const serviceAccount={
 }
 const { ObjectID } = require('mongodb')
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert({...serviceAccount}),
   databaseURL: "https://creative-agency-fullstack.firebaseio.com"
 });
 
